5e40052eda58f88b8ca548c3238d41099753ade1
Billy Zaelani Malik <bzm@pinggirjurang.studio>
2026-01-21 17:54:51 +0700

    Remove karabiner-elements in favor of simple native keyboard remap

--- configuration.nix
+++ configuration.nix
@@ -91,16 +91,9 @@ with lib;
   system.defaults.NSGlobalDomain = {
     "com.apple.swipescrolldirection" = true;
   };
-  services.karabiner-elements = {
-    enable = true;
-    package = pkgs.karabiner-elements.overrideAttrs (old: {
-      version = "14.13.0";
-      src = pkgs.fetchurl {
-        inherit (old.src) url;
-        hash = "sha256-gmJwoht/Tfm5qMecmq1N6PSAIfWOqsvuHU8VDJY8bLw=";
-      };
-      dontFixup = true;
-    });
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToEscape = true;
   };
   mod.emacs.enable = true;
 }
