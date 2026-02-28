ad6ec1dd5e6a901f93515865619447268e4bc292
Billy Zaelani Malik <bzm@pinggirjurang.studio>
2026-03-01 06:13:07 +0700

    Set git

--- users/bzm/default.nix
+++ users/bzm/default.nix
@@ -4,5 +4,20 @@
   home-manager.users.bzm =
     { ... }:
     {
+      programs.git = {
+        enable = true;
+        ignores = [
+          ".DS_Store"
+        ];
+        settings = {
+          init.defaultBranch = "main";
+          pull.rebase = true;
+          push.default = "current";
+          user = {
+            email = "bzm@pinggirjurang.studio";
+            name = "Billy Zaelani Malik";
+          };
+        };
+      };
     };
 }
