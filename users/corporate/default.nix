9cda950a3018412f25eab3827785d85ff7d542e9
Billy Zaelani Malik <bzm@pinggirjurang.studio>
2026-03-01 17:31:50 +0700

    Add git and zen-browser to corporate users

--- users/corporate/default.nix
+++ users/corporate/default.nix
@@ -1,4 +1,42 @@
 { ... }:
 
 {
+  programs.git = {
+    enable = true;
+    ignores = [
+      ".DS_Store"
+    ];
+    settings = {
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      push.default = "current";
+      user = {
+        email = "billy.malik@amartha.com";
+        name = "Billy Zaelani Malik";
+      };
+    };
+  };
+
+  programs.zen-browser = {
+    # https://github.com/0xc000022070/zen-browser-flake
+    enable = true;
+    policies = {
+      # More policies can be browse at:
+      # https://mozilla.github.io/policy-templates/
+      NoDefaultBookmarks = true;
+      DisableAppUpdate = true;
+      DisableTelemetry = true;
+    };
+
+    # TODO: the profiles is not working
+    # profiles.default = {
+    #   settings = {
+    #     "zen.welcome-screen.seen" = true;
+    #   };
+    #   search = {
+    #     force = true;
+    #     default = "google";
+    #   };
+    # };
+  };
 }
