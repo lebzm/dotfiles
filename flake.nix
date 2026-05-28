4cd26ed4bb8ab27648ef07b4c4a14813f49efc8f
Billy Zaelani Malik <bzm@pinggirjurang.studio>
2026-05-29 06:11:53 +0700

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "bzm nixos and nix-darwin configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
