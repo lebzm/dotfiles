4c99c377aa59977210426500cd11f97c14e6956e
Billy Zaelani Malik <bzm@pinggirjurang.studio>
2026-06-02 22:23:43 +0700

    Add noice and mini-icons

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -41,7 +41,17 @@ in
           colorschemes.catppuccin.enable = true;
           plugins = {
             lualine.enable = true;
-            noice.enable = true;
+            noice = {
+              enable = true;
+              settings.presets = {
+                bottom_search = true;
+                command_palette = true;
+                # long_message_to_split = false;
+                # inc_rename = false;
+                # lsp_doc_border = false;
+              };
+            };
+            mini-icons.enable = true;
           };
         };
       }
