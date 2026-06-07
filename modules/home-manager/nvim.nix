5aaa44c359af7ffe65391f2b8a31afb617e4756e
Billy Zaelani Malik <bzm@pinggirjurang.studio>
2026-06-07 19:59:02 +0700

    Add neotest + neotest-golang

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -375,6 +375,64 @@ let
     };
   };
 
+  dep_test = {
+    programs.nixvim = {
+      plugins.neotest = {
+        enable = true;
+        settings.floating.border = border_style;
+      };
+      keymaps = [
+        {
+          key = "<leader>t.";
+          action.__raw = ''function() require("neotest").run.run() end'';
+          mode = normal;
+        }
+        {
+          key = "<leader>tf";
+          action.__raw = ''function() require("neotest").run.run(vim.fn.expand("%")) end'';
+          mode = normal;
+        }
+        {
+          key = "<leader>tm";
+          action.__raw = ''function() require("neotest").run.run(vim.fn.expand("%:p:h")) end'';
+          mode = normal;
+        }
+        {
+          key = "<leader>tp";
+          action.__raw = ''function() require("neotest").run.run(vim.fn.getcwd()) end'';
+          mode = normal;
+        }
+        {
+          key = "<leader>tl";
+          action.__raw = ''function() require("neotest").run.run_last() end'';
+          mode = normal;
+        }
+        {
+          key = "<leader>tt";
+          action.__raw = ''function() require("neotest").summary.toggle() end'';
+          mode = normal;
+        }
+        {
+          key = "<leader>to";
+          action.__raw = ''
+            function()
+              require("neotest").output.open({
+                enter = false,
+                auto_close = true,
+              })
+            end
+          '';
+          mode = normal;
+        }
+        {
+          key = "<leader>ts";
+          action.__raw = ''function() require("neotest").run.stop() end'';
+          mode = normal;
+        }
+      ];
+    };
+  };
+
   lang_nix = {
     programs.nixvim = {
       plugins.conform-nvim.settings.formatters_by_ft.nix = [ "nixfmt" ];
@@ -395,6 +453,7 @@ let
       plugins.conform-nvim.settings.formatters_by_ft.go = [ "gofumpt" ];
       plugins.treesitter.grammarPackages = with treesitterGrammars; [ go ];
       lsp.servers.gopls.enable = true;
+      plugins.neotest.adapters.golang.enable = true;
     };
   };
 
@@ -589,6 +648,7 @@ in
       dep_treesitter
       dep_format
       dep_lsp
+      dep_test
 
       lang_nix
       lang_go
