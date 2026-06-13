513bfab73fee800a1efe912e87ce802c643d0098
Billy Zaelani Malik <bzm@pinggirjurang.studio>
2026-06-13 11:44:26 +0700

    Refactor out direnv and podman

---
+++ modules/home-manager/direnv.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.modules.direnv;
+in
+{
+  options.modules.direnv.enable = lib.mkEnableOption "direnv";
+
+  config = lib.mkIf cfg.enable {
+    programs.direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+  };
+}
---
+++ modules/home-manager/podman.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.modules.podman;
+in
+{
+  options.modules.podman.enable = lib.mkEnableOption "podman";
+
+  config = lib.mkIf cfg.enable {
+    services.podman = {
+      enable = true;
+      useDefaultMachine = true;
+    };
+    home.packages = [ pkgs.podman-compose ];
+    programs.zsh.shellAliases.docker = "podman";
+  };
+}
