f313917b01efd7a00baf77c81df1d4c039294130
Billy Zaelani Malik <bzm@pinggirjurang.studio>
2026-06-13 11:34:07 +0700

    Add gleam

---
+++ modules/home-manager/lang/gleam.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.modules.lang.gleam;
+  treesitterGrammars = config.programs.nixvim.plugins.treesitter.package.builtGrammars;
+in
+
+{
+  options.modules.lang.gleam.enable = lib.mkEnableOption "gleam";
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gleam
+      rebar3
+      erlang
+      bun
+    ];
+    programs.nixvim = {
+      plugins.treesitter.grammarPackages = with treesitterGrammars; [ gleam ];
+      plugins.conform-nvim.settings.formatters_by_ft.gleam = [ "gleam" ];
+      lsp.servers.gleam.enable = true;
+    };
+  };
+}
