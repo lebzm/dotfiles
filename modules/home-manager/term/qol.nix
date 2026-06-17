e12dc8c21a11b97ec1a1e7a071b3ffefe24da728
Billy Zaelani Malik <bzm@pinggirjurang.studio>
2026-06-17 12:44:14 +0700

    Add zoxide as cd replacement

--- modules/home-manager/term/qol.nix
+++ modules/home-manager/term/qol.nix
@@ -10,20 +10,35 @@ in
 {
   options.modules.term.qol.enable = lib.mkEnableOption "qol";
 
-  config = lib.mkIf cfg.enable {
-    # Better shell prompt compatible for all shell.
-    programs.starship = {
-      enable = true;
-      presets = [ "nerd-font-symbols" ];
-    };
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      # Better shell prompt compatible for all shell.
+      {
+        programs.starship = {
+          enable = true;
+          presets = [ "nerd-font-symbols" ];
+        };
+      }
 
-    # Better shell completion compatible for all shell.
-    programs.carapace.enable = true;
+      # Better shell completion compatible for all shell.
+      {
+        programs.carapace.enable = true;
+      }
 
-    # Better shell environment per project.
-    programs.direnv = {
-      enable = true;
-      nix-direnv.enable = true;
-    };
-  };
+      # Better shell environment per project.
+      {
+        programs.direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+        };
+      }
+
+      # Better cd.
+      {
+        programs.zoxide.enable = true;
+        programs.zsh.shellAliases.cd = "z";
+        programs.nushell.shellAliases.cd = "z";
+      }
+    ]
+  );
 }
